7e265f07abc6d17362ed56b313b060fe20330629
FaisalMahamudCS <fmfahimcse@gmail.com>
2022-04-04 01:30:08 +0600

    chart done

--- src/Component/DashBoard/Dashboard.js
+++ src/Component/DashBoard/Dashboard.js
@@ -1,11 +1,103 @@
 import React from 'react';
-
+import {AreaChart,Area, PieChart,Pie,LineChart,BarChart, Bar, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const Dashboard = () => {
+    const data=[
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='row'>
+                <div className="col-lg-6">
+       <LineChart width={400} height={500} data={data}>
+
+<Line dataKey={'sell'} ></Line>
+<XAxis dataKey={'month'}></XAxis>
+<YAxis dataKey={'sell'}></YAxis>
+<Tooltip></Tooltip>
+       </LineChart>
+
+       </div>
+       <div  className='col-lg-6 mt-5'>
+       <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis datakey="investment"/>
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+          <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+        </BarChart>
         </div>
-    );
+        </div>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+         
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+        
+        </AreaChart>
+        
+       </div>
+
+       );
 };
 
 export default Dashboard;
