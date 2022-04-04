6b36e21c3a40dca9b8246aed70fd11ea9af8a652
FaisalMahamudCS <fmfahimcse@gmail.com>
2022-04-04 23:22:26 +0600

    404 page updated

--- src/Component/NotFound/NotFound.js
+++ src/Component/NotFound/NotFound.js
@@ -3,53 +3,20 @@ import './NotFound.css'
 import { NavLink } from 'react-router-dom';
 const NotFound = () => {
     return (
-        <div className='pt-5 text-danger '>
-       <nav className="navbar navbar-expand-lg navbar-light ">
-       <NavLink
-          className={({ isActive }) => (isActive ? "nav-link text-danger" : "nav-link text-dark")}
-          to='/'
-        >HOME
-            </NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-    <NavLink
-          className={({ isActive }) => (isActive ? "nav-link text-danger" : "nav-link text-dark")}
-          to='/'
-        >HOME
-            </NavLink>
-            <NavLink
-            className={({ isActive }) => (isActive ? "nav-link   text-danger" : "nav-link text-dark")}
-          to='/review'
-        >REVIEWS
-            </NavLink>
-            <NavLink
-           className={({ isActive }) => (isActive ? "nav-link   text-danger" : "nav-link text-dark")}
-          to='/dashboard'
-        >DASHBOARD
-            </NavLink>
-            <NavLink
-        className={({ isActive }) => (isActive ? "nav-link   text-danger" : "nav-link text-dark")}
-          to='/blog'
-        >BLOGS
-            </NavLink>
-            <NavLink
-          className={({ isActive }) => (isActive ? "nav-link  text-danger" : "nav-link text-dark")}
-          to='/about'
-        >ABOUT
-            </NavLink>       
-    </div>
-  </div>
-</nav>
+        <div className='pt-5 text-danger  container'>
+       
 
-
-            <div className=''>
+ <div className='row '>
+            <div className='col-lg-6 pt-6'>
           <h1> 404 Not Found</h1> 
           <p>Oops This page Doesn't exist</p>
           <NavLink to='/' className='btn btn-primary px-4'>Go Back</NavLink>
+
           </div>
+          <div  className='col-lg-6'>
+          <img className='img-fluid' src="https://icon-library.com/images/mistake-icon/mistake-icon-3.jpg" alt="" srcset="" />
+        </div>
+        </div>
         </div>
     );
 };
